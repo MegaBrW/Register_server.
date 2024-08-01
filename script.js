3a2f1a9ad06c6457659053e6bcca95d3c53aaa0a
MegaBrW <111461058+MegaBrW@users.noreply.github.com>
2024-08-01 11:50:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,24 @@ document.getElementById('registerForm').addEventListener('submit', function(even
     const password = document.getElementById('password').value;
     const errorElement = document.getElementById('error');
 
-    fetch('https://register-server-logg.onrender.com/register', { // Adicionado '/register' ao final da URL
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ username, password })
-})
-.then(response => response.json())
-.then(data => {
-    if (data.message) {
-        errorElement.textContent = 'Registration successful!';
-        errorElement.style.color = 'green';
-    } else {
-        errorElement.textContent = data.error || 'Registration failed!';
-    }
-})
-.catch(error => {
-    console.error('Error:', error);
-    errorElement.textContent = 'An error occurred!';
+    fetch('https://register-server-logg.onrender.com/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.message) {
+            errorElement.textContent = 'Registration successful!';
+            errorElement.style.color = 'green';
+        } else {
+            errorElement.textContent = data.error || 'Registration failed!';
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        errorElement.textContent = 'An error occurred!';
+    });
 });
